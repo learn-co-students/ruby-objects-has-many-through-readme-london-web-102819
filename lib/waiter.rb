0163d214b8420b9ba5fa873d304386bc6883ecc1
github username <github email address>
2019-10-31 15:33:24 +0000

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,38 @@
 class Waiter
 
+    attr_accessor :name, :yrs_experience
+    @@all = []
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        meal = Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select{|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        largest_tip = 0
+        best_customer = nil
+
+        meals.each do |meal|
+            
+            if meal.tip > largest_tip
+                largest_tip = meal.tip
+                best_customer = meal.customer
+            end
+        end
+        best_customer
+    end
+
 end
